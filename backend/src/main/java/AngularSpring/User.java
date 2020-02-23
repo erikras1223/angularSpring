f13b39b83e6e0ea4d3feed1c19e933a241abb4c1
erikras1223 <razmataz1223@hotmail.com>
2020-02-23 14:40:11 -0700

    almost working build process and issues with compiling junit.

--- backend/src/main/java/AngularSpring/User.java
+++ backend/src/main/java/AngularSpring/User.java
@@ -1,9 +1,10 @@
 package AngularSpring;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+@Entity
 public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
